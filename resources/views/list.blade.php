121a2e188ead2204d43008bafb9eb3b27ca35162
Md. Bodiuzzaman Ripon <90719674+bzamanbd@users.noreply.github.com>
2022-08-10 11:06:37 +0600

    init

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -6,13 +6,23 @@
   <th>NAME</th>
   <th>USERNAME</th>
   <th>EMAIL</th>
+  <th>ADDRESS</th>
  </tr>
  @foreach ($data as $item)
+ <?php
+  $street= $item['address']['street'];
+  $suite= $item['address']['suite'];
+  $city= $item['address']['city'];
+  $zipcode= $item['address']['zipcode'];
+  $lat= $item['address']['geo']['lat'];
+  $lng= $item['address']['geo']['lng'];
+ ?>
  <tr>
   <td>{{ $item['id'] }}</td>
   <td>{{ $item['name'] }}</td>
   <td>{{ $item['username'] }}</td>
   <td>{{ $item['email'] }}</td>
+  <td>{{ $street}}, {{ $suite}}, {{ $city}}, {{ $zipcode}}, {{ $lat}}, {{ $lng}}</td>
  </tr>
  @endforeach
 </table>
